cbe54e473bc984910d375836fb296467d0543029
刘奕聪 <liuyicong@yy.com>
2014-12-03 18:30:11 -0800

    add GSimpleHTTPServer

--- GSimpleHTTPServer/__init__.py
+++ GSimpleHTTPServer/__init__.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+from gevent import monkey
+
+monkey.patch_all()
+
+import SimpleHTTPServer
+import SocketServer
+
 __author__ = 'Liu Yicong'
 __email__ = 'saber000@vip.qq.com'
+
+
+def serve_forever(port):
+    handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    httpd = SocketServer.TCPServer(("", port), handler)
+    print "[+] serving at port", port
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    import sys
+
+    port = 8000
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+
+    try:
+        serve_forever(port)
+    except KeyboardInterrupt:
+        print "[+] GSimpleHTTPServer exited"
---
+++ demo/run_at_resources.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from GSimpleHTTPServer import serve_forever
+import sys
+import os
+from os import path
+
+port = 8000
+if len(sys.argv) > 1:
+    port = int(sys.argv[1])
+
+selfpath = path.realpath(path.dirname(__file__))
+rootdir = path.join(selfpath, "resources")
+os.chdir(rootdir)
+print "[+] change directory to", rootdir
+
+try:
+    serve_forever(port)
+except KeyboardInterrupt:
+    print "[+] GSimpleHTTPServer exited"
