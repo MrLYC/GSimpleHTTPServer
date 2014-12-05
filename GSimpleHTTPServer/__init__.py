422ecdd370f149faa78b130eb898cb9540eba0a1
lyc <saber000@vip.qq.com>
2014-12-06 00:23:19 +0800

    update error message format

--- GSimpleHTTPServer/__init__.py
+++ GSimpleHTTPServer/__init__.py
@@ -5,15 +5,27 @@ from gevent import monkey
 
 monkey.patch_all()
 
-import SimpleHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
 import SocketServer
 
 __author__ = 'Liu Yicong'
 __email__ = 'saber000@vip.qq.com'
 
 
+class GSimpleHTTPServer(SimpleHTTPRequestHandler):
+    error_message_format = """
+<head>
+<title>Error response</title>
+</head>
+<body>
+<h1> %(code)d response</h1>
+<p>%(message)s : %(explain)s.</p>
+<a href="/">Index</a>
+</body>"""
+
+
 def serve_forever(port):
-    handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    handler = GSimpleHTTPServer
     httpd = SocketServer.TCPServer(("", port), handler)
     print "[+] serving at port", port
     try:
